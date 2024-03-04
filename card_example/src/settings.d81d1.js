3ad5845a0701947adf90b001f052c2ab701dec20
Bao Nguyen <baontg93@gmail.com>
2024-03-05 00:35:09 +0700

    Update build

---
+++ card_example/src/settings.d81d1.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/test.fire",orientation:"",jsList:["assets/plugins/tween-max/tween-max.min.7bdc2.js"],bundleVers:{internal:"d17e2",main:"fcb74"}};
